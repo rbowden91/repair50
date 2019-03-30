58900c97f387c05a2ef181525fdb0489fe43b63c
Rob Bowden <rbowden91@gmail.com>
2019-03-30 06:58:22 -0400

    fix setup

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
-git clone --recurse-submodules -j8 git@github.com:rbowden91/robo50
-cd pycparser
-python setup.py develop
-cd ../repair50
+cd ~/repos
+
+#git clone --recurse-submodules -j8 git@github.com:rbowden91/robo50
+cd robo50/pycparser
 python setup.py develop
 cd ../centipyde
+python setup.py develop
+cd ../repair50
 python setup.py develop
